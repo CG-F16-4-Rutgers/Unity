8cf502490725a8c2340466027867fbb616906e2b
Danny Huynh <danhuynh0803@gmail.com>
2016-10-12 13:49:59 -0400

    Current progress with first part of animation

--- Assets/Scripts/CharAnimation/CharacterController.cs
+++ Assets/Scripts/CharAnimation/CharacterController.cs
@@ -5,31 +5,97 @@ using System.Collections;
 public class CharacterController : MonoBehaviour
 {
 
-    public float speed;
+	enum PlayerModes { Idle, Moving, Reverse };
+
+    public float maxSpeed;
+	public float maxReverseSpeed;
+	public float jumpForce;
+	public float incrementSpeed;
+	public float lookSpeed; 
+
     private Rigidbody rb;
+	private Animator anim;
+	private bool canJump = true;
+	private float currentSpeed = 0.0f;
+	private Vector3 prevLoc; 
+	private Vector3 currLoc;
+	private PlayerModes currentMode; 		// Player's current state
 
     void Start()
     {
+		currentMode = PlayerModes.Idle;
         rb = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator> ();
+		currLoc = transform.position;
+		prevLoc = currLoc;
     }
 
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        float up = 300.0f;
+	void Update()
+	{
+		prevLoc = currLoc;
+		currLoc = transform.position;
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        Vector3 jump = new Vector3(0.0f, up, 0.0f);
+		switch (currentMode) {		
+		case PlayerModes.Idle:
+			if (Input.GetKeyDown (KeyCode.W)) {
+				currentMode = PlayerModes.Moving;	
+			}
 
-        rb.AddForce(movement * speed);
+			if (Input.GetKeyDown (KeyCode.S)) {
+				currentMode = PlayerModes.Reverse;
+			}
+			break;
+		case PlayerModes.Moving:
+			if (Input.GetKeyDown (KeyCode.S) || Input.GetKeyUp (KeyCode.W)) {
+				currentMode = PlayerModes.Idle;
+			}
+			break;
+		case PlayerModes.Reverse:
+			if (!Input.GetKey (KeyCode.S)) {
+				currentMode = PlayerModes.Idle;
+			}
+			break;
+		}
 
-        if (Input.GetKey("space") && rb.transform.position.y <= 0.0f)
-        {
-            rb.AddForce(jump);
-        }
+		float horizontal = Input.GetAxis ("Horizontal");
 
-    }
+		switch (currentMode) {
+		case PlayerModes.Idle:
+			currentSpeed = Mathf.Lerp (currentSpeed, 0.0f, 5.0f * Time.deltaTime);			
+			transform.RotateAround (transform.position, transform.up, horizontal * Time.deltaTime);
+			break;
+		
+		case PlayerModes.Moving: 
+			currentSpeed = Mathf.Lerp (currentSpeed, maxSpeed, Time.deltaTime);
+			break;
 
+		case PlayerModes.Reverse:
+			currentSpeed = Mathf.Lerp(currentSpeed, maxReverseSpeed, Time.deltaTime);
+			break;
+		}
+
+		anim.SetFloat ("VelocityX", 5 * horizontal);
+		anim.SetFloat ("VelocityZ", currentSpeed);
+
+		transform.position += transform.forward * currentSpeed * Time.deltaTime;   					// Move forward-reverse
+		transform.position += transform.right * horizontal * Time.deltaTime;		// Move left-right
+
+		if (Input.GetKeyDown (KeyCode.Space) && canJump) {
+			canJump = false;
+			anim.SetBool ("Jump", true);
+			rb.AddForce (new Vector3 (0.0f, jumpForce, 0.0f));
+		} else if (!canJump) {
+			anim.SetBool ("Jump", false);
+		}
+			
+		
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Ground") { 
+			canJump = true;
+		}
+	}
+		
 }
 
